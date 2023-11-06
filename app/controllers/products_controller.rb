908ef0548c830059ee9c4715faf3447b5722661e
anthony.b.chung <anthony.b.chung@gmail.com>
2023-11-06 23:12:41 +1100

    Product seed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,25 @@
 class ProductsController < ApplicationController
+  def index
+
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+  
+  end
+
+  def edit
+   @product = Product.find(params[:id])
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,51 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Product_data = Struct.new(
+  :product_id,
+  :supplier_SKU,
+  :manufacturer_SKU,
+  :short_description,
+  :product_type
+)
+
+products = []
+
+products << Product_data.new(
+  "125178",
+  "0077924038778",
+  "51012024A",
+  "Baby Q Q1200 Black assembly",
+  "Baby Q"
+)
+
+products << Product_data.new(
+  "124393",
+  "077924031403",
+  "51062024",
+  "Weber Baby Q Titanium Q1200 LP",
+  "Baby Q"
+)
+
+products << Product_data.new(
+  "124398",
+  "077924038792",
+  "54010024",
+  "Weber Q Black Q2200 LP",
+  "Q"
+)
+
+products.each do |product|
+  puts product.product_id
+
+  product_current = Product.new(
+  product_id: product.product_id,
+  supplier_SKU: product.supplier_SKU,
+  manufacturer_SKU: product.manufacturer_SKU,
+  short_description: product.short_description,
+  product_type: product.product_type
+  )
+
+  product_current.save
+end
